96a1d8e8309ea914e45a891a370d8f615c64f68b
devendra <dxcpoc001@outlook.com>
2025-06-29 10:23:22 +0000

    cpmmented out RG

--- main.tf
+++ main.tf
@@ -68,3 +68,8 @@ resource "random_integer" "suffix" {
   min = 10000
   max = 99999
 }
+
+# resource "azurerm_resource_group" "keyvault_rg" {
+#   name     = var.resource_group_name
+#   location = var.location
+# }
