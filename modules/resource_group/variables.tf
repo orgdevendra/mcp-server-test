f4b2cf0b1079c69d5025044365936351fe4f32d5
devendra <dxcpoc001@outlook.com>
2025-06-29 11:01:38 +0000

    removed rg folder

--- modules/resource_group/variables.tf
+++ modules/resource_group/variables.tf
@@ -1,1 +1,4 @@
-# This file is intentionally left blank. The custom resource_group module has been deprecated in favor of the official AVM module.
+# DEPRECATED MODULE: This file is intentionally left blank.
+# The custom resource_group module has been deprecated in favor of the official AVM module.
+# Do not use this module. Use the AVM resource group module directly in your environment files.
+# This file remains for backward compatibility and to prevent errors in legacy references.
