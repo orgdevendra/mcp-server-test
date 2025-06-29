1575661ed97eadb7414073de8237c29837bf464b
devendra <dxcpoc001@outlook.com>
2025-06-29 07:47:47 +0000

    updated the correct AVM sourcess from terrafrom registry

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -13,28 +13,28 @@ provider "azurerm" {
 }
 
 module "resource_group" {
-  source  = "Azure/resource-group/azurerm"
-  version = ">= 2.0.0"
-  resource_group_name = var.resource_group_name
-  location            = var.location
+  source  = "Azure/avm-res-group/azurerm"
+  version = ">= 0.1.0"
+  name     = var.resource_group_name
+  location = var.location
 }
 
 module "storage_account" {
-  source  = "Azure/storage-account/azurerm"
-  version = ">= 2.0.0"
+  source  = "Azure/avm-res-storageaccount/azurerm"
+  version = ">= 0.1.0"
   name                = var.storage_account_name
-  resource_group_name = module.resource_group.resource_group_name
+  resource_group_name = module.resource_group.name
   location            = module.resource_group.location
   account_tier        = var.storage_account_tier
   account_replication_type = var.storage_account_replication_type
 }
 
 module "key_vault" {
-  source  = "Azure/key-vault/azurerm"
-  version = ">= 2.0.0"
+  source  = "Azure/avm-res-keyvault/azurerm"
+  version = ">= 0.1.0"
   name                = var.key_vault_name
   location            = module.resource_group.location
-  resource_group_name = module.resource_group.resource_group_name
+  resource_group_name = module.resource_group.name
   tenant_id           = var.tenant_id
   sku_name            = var.key_vault_sku
   soft_delete_retention_days = var.key_vault_soft_delete_retention_days
@@ -42,20 +42,20 @@ module "key_vault" {
 }
 
 module "app_service_plan" {
-  source  = "Azure/app-service-plan/azurerm"
-  version = ">= 2.0.0"
+  source  = "Azure/avm-res-appserviceplan/azurerm"
+  version = ">= 0.1.0"
   name                = var.app_service_plan_name
   location            = module.resource_group.location
-  resource_group_name = module.resource_group.resource_group_name
+  resource_group_name = module.resource_group.name
   os_type             = var.app_service_plan_os_type
   sku_name            = var.app_service_plan_sku
 }
 
 module "web_app" {
-  source  = "Azure/web-app/azurerm"
-  version = ">= 2.0.0"
+  source  = "Azure/avm-res-webapp/azurerm"
+  version = ">= 0.1.0"
   name                = var.web_app_name
   location            = module.resource_group.location
-  resource_group_name = module.resource_group.resource_group_name
+  resource_group_name = module.resource_group.name
   service_plan_id     = module.app_service_plan.id
 }
