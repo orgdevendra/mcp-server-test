242a2fbc73c266d815bcfcaa9fc2e536949ce13e
devendra <dxcpoc001@outlook.com>
2025-06-29 11:04:08 +0000

    rg removed

--- modules/resource_group/README_DEPRECATED.md
+++
@@ -1,1 +0,0 @@
-# This module is deprecated. Use the official AVM resource group module directly in your environment files.
--- modules/resource_group/main.tf
+++
@@ -1,1 +0,0 @@
-# This file is intentionally left blank. The custom resource_group module has been deprecated in favor of the official AVM module.
