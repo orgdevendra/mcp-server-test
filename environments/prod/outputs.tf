185a32e3d957b9bfecb6fd3e2e1455b3212b2e4f
orgdevendra <65060947+orgdevendra@users.noreply.github.com>
2025-06-29 12:57:01 +0000

    Delete environments directory

--- environments/prod/outputs.tf
+++
@@ -1,5 +0,0 @@
-output "resource_group_name" { value = module.resource_group.name }
-output "storage_account_name" { value = module.storage_account.name }
-output "key_vault_name" { value = module.key_vault.name }
-output "app_service_plan_name" { value = module.app_service_plan.name }
-output "web_app_name" { value = module.web_app.name }
