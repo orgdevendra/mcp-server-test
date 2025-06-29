d91a10e2799bf0d3e48b33928d2a88e00fc89d86
devendra <dxcpoc001@outlook.com>
2025-06-29 08:59:58 +0000

    updated the AD authentication for storage

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -27,6 +27,7 @@ module "storage_account" {
   resource_group_name = module.resource_group.name
   account_tier        = var.storage_account_tier
   account_replication_type = var.storage_account_replication_type
+  allow_shared_key_access = false
 }
 
 module "key_vault" {
