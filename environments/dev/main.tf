f38e1cb11fe0a26385894b0bfe2c2d60a94d3b51
devendra <dxcpoc001@outlook.com>
2025-07-01 07:19:49 +0000

    updated main file for sql

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -38,3 +38,19 @@ module "key_vault" {
   soft_delete_retention_days = var.key_vault_soft_delete_retention_days
   purge_protection_enabled   = var.key_vault_purge_protection_enabled
 }
+
+module "azure_sql_database" {
+  source                  = "../../modules/azure_sql_database"
+  sql_server_name         = var.sql_server_name
+  sql_database_name       = var.sql_database_name
+  resource_group_name     = module.resource_group.name
+  location                = var.location
+  administrator_login     = var.sql_administrator_login
+  administrator_login_password = var.sql_administrator_login_password
+  collation               = var.sql_collation
+  sku_name                = var.sql_sku_name
+  max_size_gb             = var.sql_max_size_gb
+  read_scale              = var.sql_read_scale
+  zone_redundant          = var.sql_zone_redundant
+  tags                    = var.tags
+}
